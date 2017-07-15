7981a02d5e61b7340a40030d3d4c98941ae8c9dc
luyi <takamachi660@gmail.com>
2017-07-15 20:30:50 +0800

    impl LocalFile.listFiles

--- src/main/scala/com/t660/mtp_sync/LocalFile.scala
+++ src/main/scala/com/t660/mtp_sync/LocalFile.scala
@@ -1,5 +1,7 @@
 package com.t660.mtp_sync
 
+import java.io.File
+
 /**
   * Created by luyi on 15/07/2017.
   */
@@ -8,3 +10,28 @@ case class LocalFile(path: Seq[String],
                      size: Long,
                      modificationDate: Long,
                      children: Seq[LocalFile] = Seq.empty) extends IFile
+
+object LocalFile {
+
+  def listFiles(fromPath: Seq[String]): Seq[LocalFile] = {
+
+    def list(path: Seq[String])(jFile: File): LocalFile = {
+      require(jFile.exists())
+      val children = if (jFile.isDirectory) {
+        jFile.listFiles().toSeq.map(list(path))
+      } else {
+        Seq.empty
+      }
+      LocalFile(
+        path = path :+ jFile.getName,
+        isFolder = jFile.isDirectory,
+        size = jFile.length(),
+        modificationDate = jFile.lastModified(),
+        children = children
+      )
+    }
+
+    Seq(list(fromPath)(new File(fromPath.mkString("/"))))
+  }
+
+}
