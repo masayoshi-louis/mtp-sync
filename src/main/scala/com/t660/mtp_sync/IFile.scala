3e357d47b78d3874524a17ef49c81eacfe21e90f
luyi <takamachi660@gmail.com>
2017-07-16 12:58:08 +0800

    fix IFile.name()

--- src/main/scala/com/t660/mtp_sync/IFile.scala
+++ src/main/scala/com/t660/mtp_sync/IFile.scala
@@ -1,12 +1,14 @@
 package com.t660.mtp_sync
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by luyi on 15/07/2017.
   */
 trait IFile {
   def path: Seq[String]
 
-  def name: String = path.last
+  def name: String = if (path.isEmpty) "/" else path.last
 
   def isFolder: Boolean
 
@@ -25,6 +27,21 @@ object IFile {
       for (c <- file.children)
         print(c, indent + "  ")
     }
+  }
+
+  def flatten(single: IFile): Seq[IFile] = flatten(Array(single))
+
+  def flatten(list: TraversableOnce[IFile]): Seq[IFile] = {
+    val buff = new ArrayBuffer[IFile]()
+
+    def flatten(f: IFile): Unit = {
+      buff += f
+      if (f.isFolder) f.children.foreach(flatten)
+    }
+
+    list.foreach(flatten)
+
+    buff
   }
 
 }
